8de1d31328f8d506c4cd1b25998b0369d8dc2305
Gaurav <gaurav.info217@gmail.com>
2024-12-04 02:58:08 +0530

    kuch nhi

--- src/lib/gpt.ts
+++ src/lib/gpt.ts
@@ -2,7 +2,6 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-  basePath : "https://api.groq.com/openai/v1"
 });
 const openai = new OpenAIApi(configuration);
 
@@ -17,7 +16,7 @@ export async function strict_output(
   output_format: OutputFormat,
   default_category: string = "",
   output_value_only: boolean = false,
-  model: string = "gpt-4o-mini",
+  model: string = "gpt-3.5-turbo",
   temperature: number = 1,
   num_tries: number = 3,
   verbose: boolean = false
