34bf00968cbec26e1d74b227ad9bc551afc78b20
saarthak <saarthakverma739@gmail.com>
2024-12-05 07:14:21 +0530

    server synced excalidraw wrappper completed

--- src/app/api/notes/[userId]/note/[id]/route.ts
+++ src/app/api/notes/[userId]/note/[id]/route.ts
@@ -13,6 +13,29 @@ const getNoteId = (pathname: string): string | null => {
   return noteIdMatch ? noteIdMatch[1] : null;
 }
 
+export async function GET(req:NextRequest) {
+  try {
+    const userId = getUserId(req.nextUrl.pathname);
+    const noteId = getNoteId(req.nextUrl.pathname);
+    if (!userId) {
+      return new Response(JSON.stringify({ error: "userId missing" }), { status: 404 });
+    }
+    if (!noteId) {
+      return new Response(JSON.stringify({ error: "noteId missing" }), { status: 404 });
+    }
+    const data = await prisma.excalidrawNote.findFirst({
+      where: {
+        id: noteId,
+      },
+    });
+    
+    return new Response(JSON.stringify(data), { status: 200 });
+  } catch (error) {
+    console.error("Error updating data:", error);
+    return new Response(JSON.stringify({ error: "Failed to get data" }), { status: 500 });
+  }
+}
+
 export async function PATCH(req: NextRequest) {
   try {
     const userId = getUserId(req.nextUrl.pathname);
